db3b6c0f050e1c9c9052b720f4ae572545aff334
juliazbarbosa <127155774+juliazbarbosa@users.noreply.github.com>
2024-08-22 09:20:00 -0300

    Update ConexaoBD.java

--- lojalivros.java/src/ConexaoBD.java
+++ lojalivros.java/src/ConexaoBD.java
@@ -5,22 +5,40 @@ import java.sql.SQLException;
 public class ConexaoBD {
     private static ConexaoBD instancia;
     private Connection conexao;
-    private String url = "jdbc:mysql://localhost:3306/teste";
+    private String url = "jdbc:mysql://localhost:3306/lojalivros";
     private String usuario = "adm";
     private String senha = "1235";
 
 
-    public static ConexaoBD getInstancia() throws SQLException {
+    private ConexaoBD() {
+        try {
+            conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Falha ao conectar com o banco", e);
+        }
+    }
+
+
+    public static synchronized ConexaoBD getInstance() {
         if (instancia == null) {
             instancia = new ConexaoBD();
-        } else if (instancia.getConexao().isClosed()) {
-            instancia = new ConexaoBD();
         }
-
         return instancia;
     }
 
+
     public Connection getConexao() {
         return conexao;
+    }
+
+    public void fecharConexao() {
+        if (conexao != null) {
+            try {
+                conexao.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
