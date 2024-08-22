b855847f570fa7bff9a0bf8f6dfc5eaf6a247ab9
juliazbarbosa <127155774+juliazbarbosa@users.noreply.github.com>
2024-08-22 09:25:04 -0300

    Update Main.java

--- lojalivros.java/src/Main.java
+++ lojalivros.java/src/Main.java
@@ -1,3 +1,10 @@
+import DAO.AutorDAO;
+import DAO.LivroDAO;
+import Entity.Autor;
+import Entity.Livro;
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         AutorDAO autorDAO = new AutorDAO();
@@ -20,5 +27,34 @@ public class Main {
         livroDAO.inserir(livro3);
 
 
-        System.out.println("Autores:");
-        for (Autor autor : autorDAO.listar
+        List<Autor> autores = autorDAO.listarAutores();
+            System.out.println("\nAutores:");
+            for (Autor a : autores) {
+                System.out.println(a.getIdAutor() + ": " + a.getNomeAutor() + " (" + a.getNacionalidade() + ")");
+            }
+
+            List<Livro> livros = livroDAO.listarLivros();
+            System.out.println("\nTodos os livros:");
+            for (Livro l : livros) {
+                System.out.println(l.getIdLivro() + ": " + l.getTitulo() + " (" + l.getAnoPublicado() + ") - Autor ID: " + l.getAutor().getIdAutor());
+            }
+
+            
+            List<Livro> livrosDeAutor1 = livroDAO.listarLivrosDeAutor(autor1.getIdAutor());
+            System.out.println("\nLivros do autor " + autor1.getNomeAutor() + ":");
+            for (Livro l : livrosDeAutor1) {
+                System.out.println(l.getIdLivro() + ": " + l.getTitulo() + " (" + l.getAnoPublicado() + ")");
+            }
+
+         
+            List<Livro> livrosDeAutor2 = livroDAO.listarLivrosDeAutor(autor2.getIdAutor());
+            System.out.println("\nLivros do autor " + autor2.getNomeAutor() + ":");
+            for (Livro l : livrosDeAutor2) {
+                System.out.println(l.getIdLivro() + ": " + l.getTitulo() + " (" + l.getAnoPublicado() + ")");
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
